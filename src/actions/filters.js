117e6ee3061ab84ad22c4667a033c4a73711917c
Aritra1704 <aritra1704@gmail.com>
2020-09-25 16:13:55 +0530

    redux sub classification to multiple files and HOC

---
+++ src/actions/filters.js
@@ -0,0 +1,25 @@
+// Set text
+export const setTextFilter = (text) => ({
+    type: 'SET_TEXT',
+    text
+});
+
+// Sort by
+export const sortByAmount = () => ({
+    type: 'SORT_BY_AMOUNT'
+});
+
+export const sortByDate = () => ({
+    type: 'SORT_BY_DATE'
+});
+
+// Set Date
+export const setStartDate = (startDate) => ({
+    type: 'SET_START_DATE',
+    startDate
+});
+
+export const setEndDate = (endDate) => ({
+    type: 'SET_END_DATE',
+    endDate
+});
--- src/app.js
+++ src/app.js
@@ -1,8 +1,34 @@
-// Section 10 Lesson 95
+// Section 11 Lesson 101
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+const store = configureStore();
+
+store.subscribe(() => {
+    const state = store.getState();
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filter);
+    console.log(visibleExpenses);
+});
+
+const expenseOne = store.dispatch(addExpense({ 
+    description: 'Water bill', 
+    amount: 500, 
+    createdAt: -21000 
+}));
+
+const expenseTwo = store.dispatch(addExpense({ 
+    description: 'Gas Bill', 
+    amount: 700, 
+    createdAt: -1000 
+}));
+
+store.dispatch(setTextFilter('water'));
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
